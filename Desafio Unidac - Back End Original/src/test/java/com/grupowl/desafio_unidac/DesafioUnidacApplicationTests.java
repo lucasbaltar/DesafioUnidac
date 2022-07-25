9f0d77da1b7525ed003b634382643e8bd803bd84
Lucas Baltar Esnaty Boudoux <lucasesnaty@gmail.com>
2022-07-24 21:49:07 -0300

    Back End Original

---
+++ Desafio Unidac - Back End Original/src/test/java/com/grupowl/desafio_unidac/DesafioUnidacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupowl.desafio_unidac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioUnidacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
